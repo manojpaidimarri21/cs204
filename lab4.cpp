161289beef574c9d51b15328047d071955ee24ba
manojpaidimarri21 <44923214+manojpaidimarri21@users.noreply.github.com>
2019-08-26 14:51:21 +0530

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,202 +1,232 @@
+/******************************************************************************
 
-  
-#include<bits/stdc++.h>
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<vector>
+#include<stack>
+#include<string>
 using namespace std;
-
-struct etree
+string getString(char x) 
+{ 
+    string s(1, x); 
+  
+    return s;    
+} 
+/*int stoi(string s)
 {
-    string value;
-    etree* left, *right;
-};
-//This is the function for getting preference order of operators
-int pref(string c)
+	int l=s.length();
+	int b=0;
+	for(int p=0;p<l;p++)
+	{
+		int w=s[p]-48;
+		b=10*b+w;
+	}
+		return b;
+}*/
+void display(vector<string> &v)
 {
-    if(c == "^")
-    return 3;
-    else if(c == "*" || c == "/")
-    return 2;
-    else if(c == "+" || c == "-")
-    return 1;
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n" << endl;
+}
+int prec(string c) 
+{ 
+    if(c == "^") 
+    return 3; 
+    else if(c == "*" || c == "/") 
+    return 2; 
+    else if(c == "+" || c == "-") 
+    return 1; 
     else
-    return -1;
-}
-//This is the function to convert the given infix to postfix form
-vector<string> infixToPostfix(vector<string> s)
-{
-    std::stack<string> st;
-    st.push("N");
-    int l = s.size();
-    vector<string> ns;
-    for(int i = 0; i < l; i++)
-    {
-        // If the scanned character is an operand, add it to output string.
-        if(s[i]!="(" && s[i]!=")" && s[i]!="+" && s[i]!="*" && s[i]!="-" && s[i]!="/") {
-        	if(s[i]!="")
-        	ns.push_back(s[i]);
-    	}
-
-        // If the scanned character is an ‘(‘, push it to the stack.
-        else if(s[i] == "(")
-
-        st.push("(");
-
-        // If the scanned character is an ‘)’, pop and to output string from the stack
-        // until an ‘(‘ is encountered.
-        else if(s[i] == ")")
-        {
-            while(st.top() != "N" && st.top() != "(")
-            {
-                string c = st.top();
-                st.pop();
-                ns.push_back(c);
-            }
-            if(st.top() == "(")
-            {
-                st.pop();
-            }
-        }
-
-        //If an operator is scanned
-        else{
-            while(st.top() != "N" && pref(s[i]) <= pref(st.top()))
-            {
-                string c = st.top();
-                st.pop();
-                ns.push_back(c);
-            }
-            st.push(s[i]);
-        }
-
-    }
-    //Pop all the remaining elements from the stack
-    while(st.top() != "N")
-    {
-                string c = st.top();
-                st.pop();
-                ns.push_back(c);
-    }
-
+    return -1; 
+} 
+vector<string>  infixToPostfix(vector<string> s) 
+{ 
+    stack < string > st; 
+    st.push("N"); 
+    int l = s.size(); 
+    vector<string> ns; 
+    for(int i = 0; i < l; i++) 
+    { 
+        // If the scanned character is an operand, add it to output string. 
+        if(s[i]!="+"&&s[i]!="-"&&s[i]!="*"&&s[i]!="/"&&s[i]!="^"&&s[i]!="("&&s[i]!=")") 
+        ns.push_back(s[i]); 
+  
+        // If the scanned character is an (, push it to the stack. 
+        else if(s[i] == "(" )
+          
+        st.push("("); 
+          
+        // If the scanned character is an ), pop and to output string from the stack 
+        // until an ( is encountered. 
+        else if(s[i] == ")") 
+        { 
+            while(st.top() != "N" && st.top() != "(") 
+            { 
+                string c = st.top(); 
+                st.pop(); 
+               ns.push_back(c); 
+            } 
+            if(st.top() == "(") 
+            { 
+                string c = st.top(); 
+                st.pop(); 
+            } 
+        } 
+          
+        //If an operator is scanned 
+        else{ 
+            while(st.top() != "N" && prec(s[i]) <= prec(st.top())) 
+            { 
+                string c = st.top(); 
+                st.pop(); 
+                ns.push_back(c); 
+            } 
+            st.push(s[i]); 
+        } 
+  
+    } 
+    //Pop all the remaining elements from the stack 
+    while(st.top() != "N") 
+    { 
+        string c = st.top(); 
+        st.pop(); 
+        ns.push_back(c); 
+    } 
+      
     return ns;
-
-}
-//This is the function to check whether the string element is an operator or not operator
-bool isOperator(string c)
+  
+} 
+struct node
 {
-    if (c == "+" || c == "-" ||
-            c == "*" || c == "/" ||
-            c == "^")
-        return true;
-    return false;
-}
-
-etree* newNode(string node_value)
-{
-    etree *tmp = new etree;
-    tmp->left = tmp->right = NULL;
-    tmp->value = node_value;
-    return tmp;
+	string sym;
+	struct node * rchild;
+	struct node * lchild;
 };
-
-// Returns root of constructed tree for given
-// postfix expression
-etree* createTree(vector<string> postfix)
+node* getNode() 
+{ 
+    // allocating space 
+    node* newNode = new node(); 
+  
+    // inserting the required data
+    newNode->sym=" ";
+    newNode->rchild = NULL; 
+    newNode->lchild = NULL; 
+    return newNode; 
+} 
+struct node* treebuild(vector <string> s)
 {
-    stack<etree *> st;
-    etree *t, *t1, *t2;
-
-    // Traverse through every character of
-    // input expression
-    for (int i=0; i<postfix.size(); i++)
+	stack <struct node *> st;
+    int l = s.size(); 
+struct node * temp=getNode();
+struct node * t1, * t2;
+    
+    for(int i = 0; i < l; i++) 	
     {
-        // If operand, simply push into stack
-        if (!isOperator(postfix[i]))
-        {
-            t = newNode(postfix[i]);
-            st.push(t);
-        }
-        else // i.e if it is an operator
-        {
-            t = newNode(postfix[i]);
-
-            // Pop two top nodes
-            t1 = st.top(); // Store top
-            st.pop();      // Remove top
-            t2 = st.top();
-            st.pop();
-
-            //  make them children
-            t->right = t1;
-            t->left = t2;
-
-            // Add this subexpression to stack
-            st.push(t);
-        }
+    	if(s[i]!="+"&&s[i]!="-"&&s[i]!="*"&&s[i]!="/"&&s[i]!="^") 
+                {
+        	struct node * temp=getNode();
+        	temp->sym=s[i];
+        	temp->lchild=NULL;
+        	temp->rchild=NULL;
+        	st.push(temp);
+        	
+		 } 
+		 else 
+		 {      
+		 	temp->sym=s[i];
+		 	t1=st.top();
+			 temp->rchild = t1;
+			
+			 t2=st.top();
+		 	temp->lchild = t2;
+		 	st.push(temp);
+		 }
     }
-
-    //  only element will be root of expression
-    // tree
-    t = st.top();
+         temp = st.top();
     st.pop();
-
-    return t;
+	return temp;
 }
-
-int eval(etree* a){
-	if(a->left==NULL&&a->right==NULL)
-		return stoi(a->value);
-	int A = eval(a->left);
-	int B = eval(a->right);
-	if(a->value == "+")
-		return A+B;
-	else if(a->value == "-")
-		return A-B;
-	else if(a->value == "*")
-		return A*B;
-	else if(a->value == "%")
-		return A%B;
-	else if(a->value == "/")
-		return A/B;
-	else if(a->value == "^")
-		return pow(A,B);
+int evaluation(struct node *temp)
+{
+	if(temp->lchild==NULL&&temp->rchild==NULL)
+	{  string s1=temp->sym;
+	   int j =stoi(s1);
+		return j;	
+	}
+	    struct node * ltemp;
+	    ltemp=temp->lchild;
+	    struct node * rtemp;
+	    rtemp=temp->rchild;
+		int l_val=evaluation(ltemp);
+		int r_val=evaluation(rtemp);
+	if(temp->sym == "+")
+ {    
+ 	return  l_val + r_val;
+ }
+ 	else if(temp->sym == "-")
+ {
+ 	return l_val - r_val;
+ }
+	else if(temp->sym == "*")
+ {
+ 	return  l_val * r_val;
+ }
+ else if(temp->sym == "/")
+ {
+ 	return  l_val / r_val;
+ }
+ 
 }
-
+  
 int main()
 {
-    string exp;
-	cin>>exp;
-	vector<string> P;
-	string temp="";
-
-	for(int i=0; i<exp.length();i++)
-	{
-		if(exp[i]>=48 && exp[i]<=57)
-		{
-          temp+=exp[i];
+    vector<string> s;
+    char p,c;
+    cin>>p;
+    s.push_back(getString(p));
+    cin>>c;
+    while(c!='k')
+    {
+    	if(c!='+'&&c!='-'&&c!='*'&&c!='/'&&c!='('&&c!=')')
+    	{
+    		if(p=='+'||p=='-'||p=='*'||p=='/'||p=='('||p==')')
+    		{
+    			s.push_back(getString(c));
+    			p=c;
+    			cin>>c;
+			}
+			else
+			{
+				s.back()=s.back()+getString(c);
+				p=c;
+				cin>>c;
+				
+			}
+    		
 		}
 		else
 		{
-             P.push_back(temp);
-            temp=""; temp+=exp[i];
-              P.push_back(temp);
-            temp="";
+			s.push_back(getString(c));
+    			p=c;
+    			cin>>c;
+			
 		}
-
+    	
 	}
-    P.push_back(temp);
-    vector<string> postexp=infixToPostfix(P);
-    etree* a = createTree(postexp);
-    int val = eval(a);
-    cout<<val;
+	display(s);
+	vector<string> ns=infixToPostfix(s);
+	display(ns);
+	struct node *tem=NULL;
+	tem=treebuild(ns);
+	int h=evaluation(tem);
+	cout<<h;
+    return 0;
 }
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
