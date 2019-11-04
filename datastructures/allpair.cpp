9451665d725c9aac2785c82a9a67efa597343e5c
manojpaidimarri21 <44923214+manojpaidimarri21@users.noreply.github.com>
2019-11-04 13:22:15 +0530

    Update allpair.cpp

--- datastructures/allpair.cpp
+++ datastructures/allpair.cpp
@@ -1,10 +1,47 @@
+/*
+There are V number of cities in a country and E number of one way roads.
+A company want to establish a first branch in this city.Assume that raw material is available
+in every city and the only cost differ from transporting charges.
+If from a branch we can transport our products to all cities.then print "YES"(without quotes)
+and also print the sum of distance of between branch and all cities and city in which branch
+should be.
+If not possible print "No" without quotes.
+*/
 #include<bits/stdc++.h>
 using namespace std;
-#define V 4
 #define INF 10000000 
-void floydWarshall(int graph[][V],int k[][V])
-{
-	int dist[V][V];
+
+int main()
+{    int V;
+    cin>>V;
+    int E;
+    cin>>E;
+     int x;
+        int y;
+      int w;
+    int graph[V][V];
+    for(int i=0;i<V; i++)
+	    {
+			for(int j=0;j<V;j++)
+			{
+				graph[i][j]=INF;
+			}
+		}
+    
+    
+    for(int i=0;i<V;i++)
+    {
+        graph[i][i]=0;
+    }
+    for(int i=0;i<E;i++)
+    {   
+        cin>>x;
+        cin>>y;
+        cin>>w;
+        graph[x][y]=w;
+       
+    }
+    int dist[V][V];
 	for(int i=0;i<V;i++)
 	{
 		for(int j=0;j<V;j++)
@@ -24,35 +61,66 @@ void floydWarshall(int graph[][V],int k[][V])
 			}
 		}
 	}
-	for(int i=0;i<V;i++)
+   /* //prints distance matrix if needed
+   for(int i=0;i<V;i++)
 	{
 		for(int j=0;j<V;j++)
 		{
-			k[i][j]=dist[i][j];
-		}
-	}
-}
-int main()
-{
-	 int graph[V][V] = { {0, 5, INF, 10},  
-                        {INF, 0, 3, INF},  
-                        {INF, INF, 0, 1},  
-                        {INF, INF, INF, 0}  
-                    };  
-  
-     int k[V][V];
-    floydWarshall(graph,k); 
-    for(int i=0;i<V;i++)
-	{
-		for(int j=0;j<V;j++)
-		{
-			 if (k[i][j] == INF)  
+			 if (dist[i][j] == INF)  
                 cout<<"INF"<<"     ";  
             else
-                cout<<k[i][j]<<"     ";  
+                cout<<dist[i][j]<<"     ";  
 		}
 		cout<<endl;
+	}*/
+	int sum=0;
+	int i,j;
+	    for(i=0;i<V;i++)
+	{
+		for( j=0;j<V;j++)
+		{
+			if(dist[i][j]== INF)
+			{
+			    break;
+			}
+			else
+			{
+			    sum=sum+dist[i][j];
+			}
+		}
+		if(j==V)
+		{
+		    cout<<"YES"<<" value of sum is "<<sum<<" city is "<<i<<endl;
+		    break;
+		}
+		
 	}
-
+    if(i==V)
+    {
+        cout<<"NO";
+    }
     return 0;  
 }
+
+/*
+input :
+4
+4
+0 3 10
+0  1 5
+1 2 3
+2 3 1
+output:
+YES value of sum is 22 city is 0
+
+input:
+4
+4
+0 3 10
+0  1 5
+2  1 3
+2 3 1
+output:
+NO
+
+*/
