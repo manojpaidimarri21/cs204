45776aff8e9e2557be09129a78afab48798a9278
manojpaidimarri21 <44923214+manojpaidimarri21@users.noreply.github.com>
2019-11-11 14:12:37 +0530

    Update linkedlist.cpp

--- datastructures/linkedlist.cpp
+++ datastructures/linkedlist.cpp
@@ -1,160 +1,194 @@
-#include<iostream>
-#include<math.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct node
-{
-float x;
-float y;
-node *next;
+
+struct node{
+	int x;
+	struct node* next;
 };
-class list
-{
-private:
-node *head; 
-public:
-list() 
-{
-head=NULL;
 
-}
-void AddFirst(float x0,float y0)
-{
-node *temp=new node;
-temp->x=x0;
-temp->y=y0;
-if (head!=NULL){temp->next=head;} else{temp->next=NULL;}
-head=temp;
-}
-
-void display()
-{
-node *temp=new node;
-if(head!=NULL)
-{
-  temp=head;
-  while(temp!=NULL)
-  {
-  cout<<"("<<temp->x<<","<<temp->y<<")";
-  temp=temp->next;
-  }
-  cout<<endl;
-}
-else{cout<<"Empty"<<endl;}
-}
-
-
-void DelFirst()
-{
-if(head!=NULL)
-{
-node *temp=new node;
-temp=head;
-head=head->next;
-delete temp;
-}
-}
-
-void Del(float x0,float y0)
-{
-if(head->x==x0&&head->y==y0){DelFirst();cout<<"0"<<endl;return;}
-node* temp;
-temp=head;
-if(head==NULL){return ;}
-else
-{
-node* curr=new node;
-node* prev=new node;
-curr=head;
-while(1){
-if(curr->x==x0&&curr->y==y0){break;} //found
-prev=curr;
-curr=curr->next;
-if(curr==NULL){cout<<"-1"<<endl;return ;} //not found
-         }
-
-prev->next=curr->next;
-delete curr;
-cout<<"0"<<endl;
+void AddFirst(int a,struct node** start){
+	if(*start == NULL){
+		*start = new node();
+		(*start)->x = a;
+		(*start)->next = NULL;
+	}
+	else{
+		node* temp = new node();
+		temp->x =a;
+		temp->next = *start;
+		*start = temp;
+	}
 
 }
 
+void Addlast(int a,struct node** start){
+        node* temp= *start;
+        while(temp->next!=NULL)
+             {
+              temp=temp->next;
+              }
+        node* fresh= new node();
+        temp->next=fresh;
+        fresh->next=NULL;
+        fresh->x=a;
+	}
+
+void DelFirst(struct node** start){
+	if(*start == NULL){
+		cout<<"-1"<<"\n";
+	}
+	else{
+		node* temp = *start;
+    	*start = temp->next;
+    	free(temp);
+	}
+} 
+
+void Dellast(struct node** start){
+        node* temp= *start;
+        node* prev= NULL;
+        while(temp->next!=NULL)
+             {
+              prev=temp;
+              temp=temp->next;
+              }
+        prev->next=NULL;
+        free(temp);
 }
 
-long long int Length()
-{
-long long int l=0;
-node* tmp=head;
-while(tmp!=NULL)
-{
-l++;
-tmp=tmp->next;
+void Del(int a,struct node** start){
+	node* temp = *start;
+	node* prev = NULL; 
+	int flag =0;
+	while(temp!=NULL){
+		if(temp->x==a&&prev!=NULL){
+			prev->next = temp->next;
+			free(temp);
+			flag =1;
+		}
+		else if(prev == NULL&&temp->x==a){
+			DelFirst(start);
+			flag =1;
+		}
+		else{
+			prev = temp;
+			temp = temp->next;
+	}
+	}
+		if(flag ==0){
+			cout<<a<<" not found"<<endl;
+		}
 }
-cout<<l<<endl;
-return 0;
+void search(int a ,struct node** start){
+	node* temp = *start;
+	int flag = 0;
+	while(temp!= NULL){
+		if(temp->x==a){
+			flag =1;
+			break; 
+		}
+		else{
+			temp = temp->next;
+		}
+	}
+	if(flag==1){
+		cout<<a<<" exists"<<endl;
+	}
+	else{
+		cout<<a<<" doesnot exists"<<endl;
+	}
 }
 
-void Search(float x0,float y0)
+void print(struct node** start)
 {
-node* temp=head;
+node* temp=*start;
 while(temp!=NULL)
 {
-if((temp->x==x0)&&(temp->y==y0))
-{cout<<"True"<<endl;return;}
+cout<<temp->x<<" ";
 temp=temp->next;
 }
-
-{cout<<"False"<<endl;}
 }
 
-
-};
-
-
-
-int main(){
-
-list a;
-
-
-long long int T;
-cin>>T;
-while(T>0)
+void insert(int a, int b, node** start)
 {
-int x; cin>>x;
-switch(x)
-{case(1): float p,q; cin>>p>>q;
- a.AddFirst(p,q);
- break;
-case(2): a.DelFirst();
-break;
-case(3): float n,v;
-cin>>n>>v;
-a.Del(n,v);
-break;
-case(4):float c,m;cin>>c>>m;
-a.Search(c,m);
-break;
-case(5):a.Length();
-break;
-case(6):a.display();
-break;
-
+        node* temp = *start;
+	int flag = 0;
+	while(temp!= NULL){
+		if(temp->x==a){
+			flag =1;
+			break; 
+		}
+		else{
+			temp = temp->next;
+		}
+	}
+if(flag==1){
+		node* fresh= new node();
+                fresh->x=b;
+                fresh->next=temp->next;
+	        temp->next=fresh;
+            }
+	else{
+		cout<<a<<" not found"<<endl;
+	}
 }
 
-T--;
+void length(node** start){
+         node* temp = *start;
+         int count=0;
+         while(temp!=NULL)
+             {
+              count=count+1;
+              }
+         cout<<count<<endl;
 }
-
-
-return 0;
+int main(){
+	node* start = NULL;
+	int condition;
+	int T;
+	cin>>T;
+	while(T--){
+		cin>>condition;
+		if(condition==1){
+			int a;
+			cin>>a;
+			AddFirst(a,&start);
+		}
+		else if(condition ==2){
+			int a;
+			cin>>a;
+			Addlast(a,&start);
+		}
+		else if(condition == 3){
+                        DelFirst(&start);
+		}
+		else if(condition==4){
+                        Dellast(&start);
+		}
+                else if(condition==5){
+			int a;
+			cin>>a;
+                        Del(a,&start);
+                }
+                else if(condition==6){
+			int a;
+			cin>>a;
+                        search(a,&start);
+                }
+                else if(condition==6){
+                        print(&start);
+                }
+                else if(condition==7){
+                        int a,b;
+                        cin>>a;
+                        cin>>b;
+                        insert(a,b,&start);
+                }
+                else if(condition==8){
+                        length(&start);
+                }
+                else cout<<"invalid"<<endl;
+                       
+	}
+	return 0;
 }
-
-
-/*
-input:
-3
-1 1 0
-1 2 1
-6
-output:
-(2,1)(1,0)
-*/
